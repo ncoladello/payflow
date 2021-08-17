540b5f4d19eb700a3b6172cbd74075925b6dc16a
Natanael Coladello <natanael.coladello@fatec.sp.gov.br>
2021-08-17 08:35:52 -0300

    aula 04 inicio barcode

--- lib/modules/barcode_scanner/bar_code_scanner_page.dart
+++ lib/modules/barcode_scanner/bar_code_scanner_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/label_button/label_button.dart';
 
 class BarCodeScannerPage extends StatefulWidget {
   const BarCodeScannerPage({Key? key}) : super(key: key);
@@ -10,6 +13,47 @@ class BarCodeScannerPage extends StatefulWidget {
 class _BarCodeScannerPageState extends State<BarCodeScannerPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          "Escaneie o código de barras do boleto",
+          style: TextStyles.buttonBackground,
+        ),
+        leading: BackButton(
+          color: AppColors.background,
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.black,
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              color: Colors.transparent,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.black,
+            ),
+          )
+        ],
+      ),
+      bottomNavigationBar: Row(
+        children: [
+          Expanded(
+              child: LabelButton(
+                  label: "Inserir código do boleto", onPressed: () {})),
+          Expanded(
+              child:
+                  LabelButton(label: "Adicionar da galeria", onPressed: () {})),
+        ],
+      ),
+    );
   }
 }
