8660c773c929ee33b31c83ef315ca3eff524a35a
Natanael Coladello <natanael.coladello@fatec.sp.gov.br>
2021-08-20 09:01:25 -0300

    aula 4, bottomSheet improvements

--- lib/modules/barcode_scanner/bar_code_scanner_page.dart
+++ lib/modules/barcode_scanner/bar_code_scanner_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/bottom_sheet/bottom_sheet_widget.dart';
+import 'package:payflow/shared/widgets/divider_vertical/divider_vertical.dart';
 import 'package:payflow/shared/widgets/label_button/label_button.dart';
+import 'package:payflow/shared/widgets/set_label_buttons/set_label_buttons.dart';
 
 class BarCodeScannerPage extends StatefulWidget {
   const BarCodeScannerPage({Key? key}) : super(key: key);
@@ -13,46 +16,60 @@ class BarCodeScannerPage extends StatefulWidget {
 class _BarCodeScannerPageState extends State<BarCodeScannerPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text(
-          "Escaneie o código de barras do boleto",
-          style: TextStyles.buttonBackground,
-        ),
-        leading: BackButton(
-          color: AppColors.background,
-        ),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(
-              color: Colors.black,
+    return BottomSheetWidget(
+      title: 'Não foi possível identificar um código de barras',
+      subtitle: 'Tente escanear novamente ou digite o código do seu boleto.',
+      primaryLabel: 'Escanear novamente',
+      primaryOnPressed: () {},
+      secondaryLabel: 'Digitar código',
+      secondaryOnPressed: () {},
+    );
+    return SafeArea(
+      top: true,
+      bottom: true,
+      left: true,
+      right: true,
+      child: RotatedBox(
+        quarterTurns: 1,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            title: Text(
+              "Escaneie o código de barras do boleto",
+              style: TextStyles.buttonBackground,
+            ),
+            centerTitle: true,
+            leading: BackButton(
+              color: AppColors.background,
             ),
           ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              color: Colors.transparent,
-            ),
+          body: Column(
+            children: [
+              Expanded(
+                child: Container(
+                  color: Colors.black.withOpacity(0.6),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  color: Colors.transparent,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.black.withOpacity(0.6),
+                ),
+              )
+            ],
           ),
-          Expanded(
-            child: Container(
-              color: Colors.black,
-            ),
-          )
-        ],
-      ),
-      bottomNavigationBar: Row(
-        children: [
-          Expanded(
-              child: LabelButton(
-                  label: "Inserir código do boleto", onPressed: () {})),
-          Expanded(
-              child:
-                  LabelButton(label: "Adicionar da galeria", onPressed: () {})),
-        ],
+          bottomNavigationBar: SetLabelButtons(
+            primaryLabel: "Inserir código do boleto",
+            primaryOnPressed: () {},
+            secondaryLabel: "Adicionar da galeria",
+            secondaryOnPressed: () {},
+          ),
+        ),
       ),
     );
   }
